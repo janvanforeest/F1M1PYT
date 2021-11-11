9cb9e27f35e4897c7de2952509bd9c271e6d78b1
jan van foreest <30220@ma-web.nl>
2021-11-11 11:57:50 +0100

    1.1

---
+++ PYTB1L2MyFirstOperations/myfirstoperations.py
@@ -0,0 +1,16 @@
+a = 4
+b = 1 + 5
+c = 4 - 3
+d = 2 * 2
+e = 8 / 2
+f = 11 % 5
+g = 9 ** 9
+h = 40 // 3
+print("de = operator geeft antwoorden  a=4 : " ,str(a) )
+print("de + operator telt cijfers op 1+5=" ,str(b) )
+print("de - operator trekt cijfers af 4-3=" ,str(c) )
+print("de * operator keert een cijfer een pebaald keer op zichzelf op 4-3= " ,str(d) )
+print("de / operator deelt sommen door elkaar heen 8/2=" ,str(e) )
+print("de % opertor geeft de rest aan 11 % 5=" ,str(f))
+print("de ** operator exponenteert cijfers 3**3=" ,str(g))
+print("de // operator deelt door met een precies antwoord 40//3=" ,str(h))
---
+++ PYTB1L2PlayerStatistics/playerstatistics.py
@@ -0,0 +1,41 @@
+
+player ="Jan van Foreest"
+
+print("player :" + player)
+
+
+leeftijd = 17
+
+print("leeftijd :" + str(leeftijd))
+
+levend = True
+
+print("levend :" + str(levend))
+
+broertjes ="willem en frederijk"
+
+print("broertje :" + broertjes)
+
+lengte = 1.78
+
+print("lengte :" + str(lengte))
+
+kleur = "groen"
+
+print("favoriete kleur :" + kleur)
+
+geboortejaar = 2004
+
+print("geboorte jaar :" + str(geboortejaar))
+
+geslacht = "man"
+
+print("geslacht :" + geslacht)
+
+datum = 7.15
+
+print("geboortedatum :" + str(datum))
+
+land = "Nederland"
+
+print("geboorteland :" + land)
